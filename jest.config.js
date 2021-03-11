b24029c98cc82a6abf417160a7b06f23476b4555
Marcelo Savian <marcelo.savian@gmail.com>
2021-03-11 10:19:55 -0300

    test: added test features

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,14 @@
-export default {
+module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
   }
 }
